2f87cbe7a2afa48c21b10ae602d497db8f0aee15
Vinicius Ferreira <67122007+vinifm@users.noreply.github.com>
2021-09-17 17:08:47 -0300

    update functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 16:41:09 by viferrei          #+#    #+#             */
-/*   Updated: 2021/09/16 17:22:54 by viferrei         ###   ########.fr       */
+/*   Updated: 2021/09/17 17:04:58 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,18 @@
 #include <unistd.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1000
+# define BUFFER_SIZE 48
 #endif
 
 char	*get_next_line(int fd);
-char	*find_nl(int fd, char *s_buff, char *buffer);
+char	*find_nl(int fd, char *excess, char *buffer);
 
 /* utils */
 char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *str, int c);
 size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif

bf67bf67d1a83bf8fa7a22ae3653f715cae8ca3b
Vinicius Ferreira <67122007+vinifm@users.noreply.github.com>
2021-10-04 15:33:49 -0300

    add main, Makefile and txt file

---
+++ liveshare/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/13 16:11:46 by viferrei          #+#    #+#             */
+/*   Updated: 2021/10/04 15:23:18 by viferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#include "get_next_line.h"
+
+int	main(void)
+{
+	int		fd;
+	int		n;
+	int		index;
+
+	fd = open ("text.txt", O_RDONLY);
+	//fd = open ("empty", O_RDONLY);
+	index = 1;
+	n = 11;
+	while (n)
+	{
+		printf("%d >>%s<<", index, get_next_line(fd));
+		n--;
+		index++;
+	}
+	return (0);
+}
